1efeef70543754fc8d362cf26c39d44448b8c7d6
eyezah <50851883+eyezahhhh@users.noreply.github.com>
2023-07-04 15:08:45 +1200

    remove context specific exports

--- src/Index.ts
+++ src/Index.ts
@@ -14,6 +14,3 @@ export * from "./interface/PlayerModelOptions.js"
 export * from "./model/ModelPart.js"
 export * from "./model/PlayerModel.js"
 export * from "./Utils.js"
-
-export * from "./platformSpecifics/ClientPlatformUtils.js"
-export * from "./platformSpecifics/ServerPlatformUtils.js"
