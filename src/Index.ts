27691504f099d301a24f0f3eec2619b65001809b
Isaiah Meek <50851883+eyezahhhh@users.noreply.github.com>
2023-11-14 16:00:10 +1300

    add disposable object export

--- src/Index.ts
+++ src/Index.ts
@@ -2,18 +2,19 @@ import MantleRenderer from "./MantleRenderer.js"
 
 export default MantleRenderer;
 
-export * from "./interface/RendererOptions.js"
-export * from "./interface/LightOptions.js"
 export * from "./ModelUtils.js"
-export * from "./interface/GenericModel.js"
+export * from "./Utils.js"
+
+export * from "./interface/DisposableObject.js"
 export * from "./interface/GenericModel.js"
 export * from "./interface/LightOptions.js"
 export * from "./interface/ModelInfo.js"
 export * from "./interface/ModelPartParent.js"
 export * from "./interface/PlayerModelOptions.js"
+export * from "./interface/RendererOptions.js"
+
 export * from "./model/ModelPart.js"
 export * from "./model/PlayerModel.js"
-export * from "./Utils.js"
 
 export { ServerPlatformUtils } from "./platformSpecifics/ServerPlatformUtils.js";
 export { ClientPlatformUtils } from "./platformSpecifics/ClientPlatformUtils.js";
